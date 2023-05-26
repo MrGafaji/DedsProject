f2da52c74068fc2946986a72351b5b9f5616f3c1
BritneyPaul <bpaul0122@gmail.com>
2023-05-26 12:30:57 +0200

    test for regio pagina

--- mysite/testing/cypress/e2e/spec.cy.js
+++ mysite/testing/cypress/e2e/spec.cy.js
@@ -3,3 +3,18 @@ describe('connectie controleren', () => {
     cy.visit('http://127.0.0.1:8000/')
   })
 })
+
+describe('Tests van int btns', () => {
+  it('should submit the form', () => {
+    // Visit the page that contains the form
+    cy.visit('http://127.0.0.1:8000/');
+
+    //Click the Regio button
+    cy.get(`a[href="/1/"]`).should('have.text', "Regio's").click();
+    // Click the submit button
+    cy.get('input[type="submit"][value="haal statistieken op"]').click();
+
+    // Assert the correct page 
+    cy.url().should('include', '/1/statistieken') // => true
+  });
+});
