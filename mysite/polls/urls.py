33a309e9b00fc57cb4223538333a34ed9443c6ef
Skleijn02 <21052468@student.hhs.nl>
2023-05-26 17:55:12 +0200

    Dashboard
    Dashboard krijgt live data binnen van lokaal opgeslagen acces database

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from polls.DataConversions import SalesPerRegion as Sales
 
 app_name = "polls"
 urlpatterns = [
@@ -7,4 +8,6 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/statistieken", views.vote, name="statistieken"),
+    path('getSalesAmountPerRegion/', Sales.get_sales_amount_per_region, name='getSalesAmountPerRegion'),
+    path('getSalesPerRegion/', Sales.get_sales_per_region, name='getSalesPerRegion'),
 ]
