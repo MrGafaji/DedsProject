906585dc0b97636278d953c6e7ebaad6cdefa0ab
Skleijn02 <21052468@student.hhs.nl>
2023-05-30 13:18:47 +0200

    dashboard region
    dashboard regio met connectie naar aenc acces db

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from polls.DataConversions import SalesPerRegion as Sales
+from polls.DataConversions import AenC as Sales
 
 app_name = "polls"
 urlpatterns = [
@@ -8,6 +8,11 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/statistieken", views.vote, name="statistieken"),
+
     path('getSalesAmountPerRegion/', Sales.get_sales_amount_per_region, name='getSalesAmountPerRegion'),
     path('getSalesPerRegion/', Sales.get_sales_per_region, name='getSalesPerRegion'),
+    path('getRegions/', Sales.get_regions, name='getRegions'),
+    path('getYears/', Sales.get_years, name='getYears'),
+    path('getSalesPerRegionPerMonth/<str:region>/<int:year>/', Sales.get_sales_per_region_per_month, name='getSalesPerRegionPerMonth'),
+    path('getBestSoldProductInProductCategory/', Sales.get_best_sold_product_in_product_category, name='getBestSoldProductInProductCategory'),
 ]
