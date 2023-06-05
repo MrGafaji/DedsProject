75792840ef69eebe48b839a69cbefd39a0ff1217
Blaxy04 <101121626+Blaxy04@users.noreply.github.com>
2023-06-05 15:08:20 +0200

    Revert "fix hosts"
    This reverts commit de2b279b8289a3274d6c289e7efb0c9499d8fa12.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t3##wbfjl1g%kix4mzcm2j(yswjy#n7zjdmg7&gilms!1fcc$='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
