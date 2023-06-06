20b2a6e302453c51ceed70a6468ddf7e9223c336
Blaxy04 <101121626+Blaxy04@users.noreply.github.com>
2023-06-06 16:41:20 +0200

    ...

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "mysite/polls/static/"
+STATIC_ROOT = "polls/static/"
+STATIC_URL = "static/"
 
 
 # Default primary key field type
