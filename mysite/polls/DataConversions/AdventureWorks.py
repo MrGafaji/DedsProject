c555d591e913b3d3a140cc9ea035034c9c88b730
Skleijn02 <21052468@student.hhs.nl>
2023-06-06 16:28:10 +0200

    as
    as

--- mysite/polls/DataConversions/AdventureWorks.py
+++ mysite/polls/DataConversions/AdventureWorks.py
@@ -7,17 +7,27 @@ Vendor = DBConn.toDf(DBConn.vendorSUP)
 
 merged_data = pd.merge(productVendor, Vendor, left_on='BusinessEntityID', right_on='BusinessEntityID')
 
-def get_amount_sold_products_per_supplier(request):
+def get_qty_sold_products_per_supplier(request):
     qty_sold_products_per_supplier = merged_data.groupby('Name')['OnOrderQty'].sum().reset_index()
+    qty_sold_products_per_supplier = qty_sold_products_per_supplier.sort_values(by='OnOrderQty', ascending=False)
+    qty_sold_products_per_supplier = qty_sold_products_per_supplier[qty_sold_products_per_supplier['OnOrderQty'] > 0]
     qty_sold_products_per_supplier.rename(columns={'OnOrderQty': 'total_sales_quantity'}, inplace=True)
-    qty_sold_products_per_supplier = qty_sold_products_per_supplier[qty_sold_products_per_supplier['total_sales_quantity'] != 0]
 
     result = {
-        'amount_sold_products_per_supplier': qty_sold_products_per_supplier.to_dict('records')
+        'qty_sold_products_per_supplier': qty_sold_products_per_supplier.to_dict('records')
     }
     
     return JsonResponse(result, safe=False)
 
+def get_amount_money_per_supplier(request):
+    amount_money_per_supplier = merged_data.groupby('Name').apply(lambda x: (x['OnOrderQty'] * x['LastReceiptCost']).sum()).reset_index(name='total_amount_money')
+    amount_money_per_supplier = amount_money_per_supplier.sort_values(by='total_amount_money', ascending=False)
+    amount_money_per_supplier = amount_money_per_supplier[amount_money_per_supplier['total_amount_money'] > 0]
+
+    result = {
+        'amount_money_per_supplier': amount_money_per_supplier.to_dict('records')
+    }
+
+    return JsonResponse(result, safe=False)
+
 DBConn.supabase.auth.sign_out()
-
-
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -27,8 +27,9 @@ urlpatterns = [
     path('getmostprofitpercustomergroup/', Bike.get_most_profit_per_customergroup, name='get_most_profit_per_customergroup'),
     path('getBestSoldProductInProductCategoryBike/', Bike.get_best_sold_product_in_product_category, name='getBestSoldProductInProductCategoryBike'),
     ##supplier
-    path('getAmountSoldProductsPerSupplier/', AdventureWorks.get_amount_sold_products_per_supplier, name='getAmountSoldProductsPerSupplier'),
+    path('getQtySoldProductsPerSupplier/', AdventureWorks.get_qty_sold_products_per_supplier, name='getQtySoldProductsPerSupplier'),
+    path('getAmountMoneyPerSupplier/', AdventureWorks.get_amount_money_per_supplier, name='getAmountMoneyPerSupplier'),
 
-    
+
     path('predict_bonus/', RandomForest.predict_bonus, name='predict_bonus')
 ]
