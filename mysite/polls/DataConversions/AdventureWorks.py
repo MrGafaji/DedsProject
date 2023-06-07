d04ee597df143cf090338102a547b5932dbda972
Skleijn02 <21052468@student.hhs.nl>
2023-06-07 11:33:45 +0200

    as
    as

--- mysite/polls/DataConversions/AdventureWorks.py
+++ mysite/polls/DataConversions/AdventureWorks.py
@@ -7,7 +7,7 @@ Vendor = DBConn.toDf(DBConn.vendorSUP)
 
 merged_data = pd.merge(productVendor, Vendor, left_on='BusinessEntityID', right_on='BusinessEntityID')
 
-def get_amount_sold_products_per_supplier(request):
+def get_qty_sold_products_per_supplier(request):
     amount_sold_products_per_supplier = merged_data.groupby('Name')['StandardPrice'].sum().reset_index()
     amount_sold_products_per_supplier.rename(columns={'StandardPrice': 'total_sales_amount'}, inplace=True)
 
