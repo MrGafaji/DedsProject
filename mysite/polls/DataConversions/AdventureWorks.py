d97054a33dfe870c8602c621f1ca5eec812c2fc2
Skleijn02 <21052468@student.hhs.nl>
2023-06-07 16:21:50 +0200

    as
    as

--- mysite/polls/DataConversions/AdventureWorks.py
+++ mysite/polls/DataConversions/AdventureWorks.py
@@ -6,9 +6,16 @@ productVendor = DBConn.toDf(DBConn.productVendorSUP)
 Vendor = DBConn.toDf(DBConn.vendorSUP)
 SalesOrderDetail = DBConn.toDf(DBConn.salesOrderDetailSUP)
 ProductionProduct = DBConn.toDf(DBConn.productionProductSUP)
+WindOrderDetails = DBConn.toDf(DBConn.WindOrderDetailsSUP)
+WindCategory = DBConn.toDf(DBConn.WindCategorySUP)
+Product1 = DBConn.toDf(DBConn.Product1SUP)
+
+
 
 merged_data = pd.merge(productVendor, Vendor, left_on='BusinessEntityID', right_on='BusinessEntityID')
 merged_data2 = pd.merge(SalesOrderDetail, ProductionProduct, on='ProductID')
+merged_dataNorthwind = pd.merge(Product1, WindCategory, on='CategoryID')
+merged_dataNorthwind = pd.merge(merged_dataNorthwind, WindOrderDetails, on='ProductID')
 
 def get_qty_sold_products_per_supplier(request):
     qty_sold_products_per_supplier = merged_data.groupby('Name')['OnOrderQty'].sum().reset_index()
@@ -42,6 +49,21 @@ def get_most_sold_products(request):
 
     result = {
         'most_sold_products': most_sold_products.to_dict('records')
+    }
+
+    return JsonResponse(result, safe=False)
+
+
+##Northwind
+def get_best_sold_product_in_product_category(request):
+    most_sold_products_per_product_category = merged_dataNorthwind.groupby('CategoryName')['Quantity'].sum().reset_index()
+    most_sold_products_per_product_category = most_sold_products_per_product_category.sort_values(by='Quantity', ascending=False)
+    most_sold_products_per_product_category = most_sold_products_per_product_category.head(2)
+    most_sold_products_per_product_category = most_sold_products_per_product_category[most_sold_products_per_product_category['Quantity'] > 0]
+    most_sold_products_per_product_category.rename(columns={'Quantity': 'total_sales_quantity'}, inplace=True)
+
+    result = {
+        'most_sold_products_per_product_category': most_sold_products_per_product_category.to_dict('records')
     }
 
     return JsonResponse(result, safe=False)
